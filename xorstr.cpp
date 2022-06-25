36987c26dfea17f5359361b5ec2bd1603ac7b654
Keramis <81401952+Keramis@users.noreply.github.com>
2022-06-24 18:25:13 -0700

    User can now set multiple keys.

--- xorstr.cpp
+++ xorstr.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <iostream>
 #include <bitset>
+#include <vector>
 
 /*
 Mission: To XOR each character of a string.
@@ -55,3 +56,43 @@ std::string xorStringPerChar(std::string input, std::string eightBitKey)
 	return retString;
 }
 //xor's both ways!
+
+/*
+New mission:
+-XOR using multiple keys.
+How-to:
+-Vector with said keys (user inputs)
+-Get size of vector
+-Loop through the string through the keys of the vector
+-Save said strings, we're gonna need them to decrypt.
+--
+Make functions to:
+-Encrypt
+-Decrypt
+...using a vector length and for loops.
+*/
+
+std::vector<std::string> userKeys{"10110010", "11001010", "00110101", "11001100", "10101010"}; //these will be custom-set.
+
+std::string encrypted_string_iteration(std::string mainString, std::vector<std::string> inputKeys)
+{
+	std::string last_back = mainString;
+	for (int i = 0; i < inputKeys.size(); i++)
+	{
+		last_back = (xorStringPerChar(last_back, inputKeys[i])); //element at end of vector
+		std::cout << last_back << '\n';
+	}
+	std::cout << "Returning " << last_back << '\n';
+	return last_back;
+}
+std::string decrypted_string_iteration(std::string encrypted_string, std::vector<std::string> inputKeys)
+{
+	std::string last_back = encrypted_string;
+	for (auto i = inputKeys.size(); i > 0; i--)
+	{
+		last_back = xorStringPerChar(last_back, inputKeys[i]);
+		std::cout << last_back << '\n';
+	}
+	std::cout << "Returning " << last_back << '\n';
+	return last_back;
+}
