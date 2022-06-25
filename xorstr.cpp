3b265ffe6fbcac69b68f33911a2158aaa3020dc2
Keramis <81401952+Keramis@users.noreply.github.com>
2022-06-24 18:36:20 -0700

    bug + cout + example

--- xorstr.cpp
+++ xorstr.cpp
@@ -72,7 +72,7 @@ Make functions to:
 ...using a vector length and for loops.
 */
 
-std::vector<std::string> userKeys{"10110010", "11001010", "00110101", "11001100", "10101010"}; //these will be custom-set.
+std::vector<std::string> userKeys{"10110010", "11001010", "00110101"}; //these will be custom-set.
 
 std::string encrypted_string_iteration(std::string mainString, std::vector<std::string> inputKeys)
 {
@@ -80,19 +80,27 @@ std::string encrypted_string_iteration(std::string mainString, std::vector<std::
 	for (int i = 0; i < inputKeys.size(); i++)
 	{
 		last_back = (xorStringPerChar(last_back, inputKeys[i])); //element at end of vector
-		std::cout << last_back << '\n';
+		//std::cout << last_back << '\n';
 	}
-	std::cout << "Returning " << last_back << '\n';
+	//std::cout << "Returning " << last_back << '\n';
 	return last_back;
 }
 std::string decrypted_string_iteration(std::string encrypted_string, std::vector<std::string> inputKeys)
 {
 	std::string last_back = encrypted_string;
-	for (auto i = inputKeys.size(); i > 0; i--)
+	for (size_t i = inputKeys.size(); i > 0; i--)
 	{
 		last_back = xorStringPerChar(last_back, inputKeys[i]);
-		std::cout << last_back << '\n';
+		//std::cout << last_back << '\n';
 	}
-	std::cout << "Returning " << last_back << '\n';
+	//std::cout << "Returning " << last_back << '\n';
 	return last_back;
+}
+
+
+int main()
+{
+	auto a = encrypted_string_iteration("This is an encryped string!", userKeys);
+	std::cout << '\n\n';
+	auto b = encrypted_string_iteration(a, userKeys);
 }
